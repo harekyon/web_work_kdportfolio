68c0a1f3770f5416af743a72c74a3d84e2d5a787
harekyon <harekyon@gmail.com>
2023-05-07 23:10:17 +0900

    画像エリアでのnext,prev制御完了

--- components/Slider.js
+++ components/Slider.js
@@ -1,15 +1,59 @@
 import styles from "./Slider.module.scss";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { useEffect, useRef } from "react";
 export default function Slider({ imgObj, imgState }) {
+  const ref = useRef();
+  useEffect(() => {
+    console.log(ref.current);
+  }, []);
   return (
     <>
       <div className={styles["slider--wrap"]}>
-        {imgObj.map((i) => {
+        {/* <div className={styles["slider--button-wrap"]}>
+          <div className={styles["slider__prev-button"]}></div>
+          <div className={styles["slider__next-button"]}></div>
+        </div> */}
+
+        <Splide
+          options={{
+            loop: true,
+            type: "fade",
+            rewind: true,
+            autoplay: true,
+            pauseOnHover: false,
+            interval: 5000,
+          }}
+          aria-label="お気に入りの写真"
+          ref={ref}
+        >
+          {imgObj.map((i, idx) => {
+            return (
+              <SplideSlide>
+                <img
+                  key={idx}
+                  className={styles["slider__img"]}
+                  src={i.img}
+                  alt={i.alt}
+                />
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+        {/* {imgObj.map((i) => {
           return (
             // <div className={styles[""]}>
             <img className={styles["slider__img"]} src={i.img} alt={i.alt} />
             // </div>
           );
-        })}
+        })} */}
+
+        {/* <SplideSlide>
+          <img src="image1.jpg" alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="image2.jpg" alt="Image 2" />
+        </SplideSlide> */}
       </div>
     </>
   );
