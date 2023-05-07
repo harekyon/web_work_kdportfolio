b7b730e89295af3c46b62e3336e73d9b3f92858e
harekyon <harekyon@gmail.com>
2023-05-07 22:12:33 +0900

    スライダー途中

--- components/Slider.js
+++ components/Slider.js
@@ -1,23 +1,15 @@
 import styles from "./Slider.module.scss";
-export default function Slider() {
+export default function Slider({ imgObj, imgState }) {
   return (
     <>
       <div className={styles["slider--wrap"]}>
-        <img
-          className={styles["slider__img"]}
-          src="https://images.squarespace-cdn.com/content/v1/5a2f39bba9db09ee7335b85a/1565301606403-55ZAEF5FN07C8AA039TI/07_S3K6333.jpg"
-          alt="かわいい猫の画像 part1"
-        />
-        <img
-          className={styles["slider__img"]}
-          src="https://images.squarespace-cdn.com/content/v1/5a2f39bba9db09ee7335b85a/1565301591657-5FFNAHHAJICJWKLAZ9HV/01_S3K6237.jpg"
-          alt="かわいい猫の画像 part1"
-        />
-        <img
-          className={styles["slider__img"]}
-          src="https://www.pakutaso.com/shared/img/thumb/shikun20220402_114719-2_TP_V.jpg"
-          alt="かわいい猫の画像 part1"
-        />
+        {imgObj.map((i) => {
+          return (
+            // <div className={styles[""]}>
+            <img className={styles["slider__img"]} src={i.img} alt={i.alt} />
+            // </div>
+          );
+        })}
       </div>
     </>
   );
